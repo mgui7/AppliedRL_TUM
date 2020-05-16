d0e30a8376f8a0eea6f09a7a32f6bfecc14f0954
kaiyikang <kaiyikang@gmail.com>
2020-05-16 23:45:25 +0200

    Draw the basic elements

---
+++ elements.py
@@ -0,0 +1,54 @@
+import pyglet
+from gym.envs.classic_control import rendering
+
+"""
+Introduction of this modules
+
+This module draws some static elements, start point and target, or each trajectory on the map. 
+Attention, the score points are not attracted by this module because they will disappear after 
+the end of arm2 reach the score point. 
+
+"""
+
+# Size of screen
+SCREEN_W = 600
+SCREEN_H = 600
+
+# The color of start and end point
+START_POINT_COLOR = (255/255,0,0)
+END_POINT_COLOR  = (0,255/255,0)
+
+class TrackLine():
+    """ The trajectory of movement of robot.
+    """
+    def __init__(self,viewer):
+        """
+        Args:
+            viewer: the current viewer of the program. A element should be attached on this viewer.
+        """
+        self.viewer = viewer
+
+    def draw_line_track(self):
+        self.track = rendering.Line((SCREEN_W/2,20), (SCREEN_W/2,SCREEN_H-20))
+        self.track.set_color(0,0,0)
+        self.viewer.add_geom(self.track)
+
+    
+class StartEndPoint():
+    """ The start and end point on the map
+    """
+    def __init__(self,viewer):
+        self.viewer = viewer
+
+    def draw_points(self):
+        start = rendering.make_circle(10,10)
+        start_transform = rendering.Transform(translation=(300,20))
+        start.add_attr(start_transform)
+        start.set_color(START_POINT_COLOR[0],START_POINT_COLOR[1],START_POINT_COLOR[2])
+        self.viewer.add_geom(start)
+
+        end = rendering.make_circle(10,10)
+        end_transform = rendering.Transform(translation=(300,580))
+        end.add_attr(end_transform)
+        end.set_color(END_POINT_COLOR[0],END_POINT_COLOR[1],END_POINT_COLOR[2])
+        self.viewer.add_geom(end)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import gym
 from gym.utils import seeding
 from gym import spaces, logger
+import elements
 
 # generate ramdom seed 
 import random
@@ -12,11 +13,19 @@ import numpy as np
 # to control the time delay
 import time
 
-
+# Size of screen
 SCREEN_W, SCREEN_H = 600,600
 
 MAX_EPISODES = 10
 MAX_EP_STEPS = 200
+
+# Size of Arm 
+ARM_W = 10.0
+ARM_LEN = 150
+
+# Size of robot
+ROBOT_W = 40.0
+ROBOT_H = 40.0
 
 
 # ENV CLASS
@@ -82,8 +91,90 @@ class MainEnv(gym.Env):
         if self.viewer is None:
             # draw the elements by rendering module
             from gym.envs.classic_control import rendering
+
             # create a viewer
             self.viewer = rendering.Viewer(SCREEN_W, SCREEN_H)
+
+            # Create a trackline 
+            # track = elements.TrackLine(self.viewer)
+            # track.draw_line_track()
+
+            # Create a start and end point 
+            start_end_points = elements.StartEndPoint(self.viewer)
+            start_end_points.draw_points()
+
+            # Create a score1 
+            self.score1 = rendering.make_circle(10)
+            self.score1_trans = rendering.Transform(translation=(150, 450))
+            self.score1.add_attr(self.score1_trans)
+            self.score1.set_color(255 / 255, 192 / 255, 0)
+            self.viewer.add_geom(self.score1)
+
+            # Create a score2
+            self.score2 = rendering.make_circle(10)
+            self.score2_trans = rendering.Transform(translation=(100, 150))
+            self.score2.add_attr(self.score2_trans)
+            self.score2.set_color(255 / 255, 192 / 255, 0)
+            self.viewer.add_geom(self.score2)
+
+            # Create a score3
+            self.score3 = rendering.make_circle(10)
+            self.score3_trans = rendering.Transform(translation=(450, 200))
+            self.score3.add_attr(self.score3_trans)
+            self.score3.set_color(255 / 255, 192 / 255, 0)
+            self.viewer.add_geom(self.score3)
+
+            # Create a robot
+            l, r, t, b = -ROBOT_W / 2, ROBOT_W / 2, ROBOT_H / 2, -ROBOT_H / 2 # l is left, r is right, t is top and b is bottom
+            robot = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)]) # draw and fill the polygon
+            self.robot_trans = rendering.Transform() # add the Transform to the robot 
+            robot.add_attr(self.robot_trans) 
+            self.viewer.add_geom(robot) # add the robot into the viewer
+
+            # Create the first arm
+            l, r, t, b = -ARM_W / 2, ARM_W / 2, ARM_LEN - ARM_W / 2, -ARM_W / 2
+            arm1 = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
+            arm1.set_color(.8, .6, .4)
+            self.arm1_trans = rendering.Transform()
+
+            # The arm1 and robot move together
+            arm1.add_attr(self.arm1_trans)
+            arm1.add_attr(self.robot_trans)
+            self.viewer.add_geom(arm1)
+
+            # Create the second arm
+            l, r, t, b = -ARM_W / 2, ARM_W / 2, ARM_LEN - ARM_W / 2 * 2, -ARM_W / 2
+            arm2 = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
+            arm2.set_color(.8, .6, .4)
+            self.arm2_trans = rendering.Transform(translation=(0, ARM_LEN - ARM_W / 2)) # Attention the initial translation
+
+            # arm2 moves with the arm1 and the robot.
+            arm2.add_attr(self.arm2_trans)
+            arm2.add_attr(self.arm1_trans)
+            arm2.add_attr(self.robot_trans)
+            self.viewer.add_geom(arm2)
+
+            # add the joint1 
+            self.joint1 = rendering.make_circle(ARM_W / 2)
+            self.joint1.add_attr(self.arm1_trans)
+            self.joint1.add_attr(self.robot_trans)
+            self.joint1.set_color(.5, .5, .8)
+            self.viewer.add_geom(self.joint1)
+
+            # add the joint2 
+            # self.joint2 = rendering.make_circle(ARM_W / 2)
+            # self.joint2.add_attr(self.arm1_trans)
+            # self.joint2.add_attr(self.arm2_trans)
+            # self.joint2.set_color(.5, .5, .8)
+            # self.viewer.add_geom(self.joint2)
+
+            # add the joint3 
+            self.joint3 = rendering.make_circle(ARM_W / 2)
+            self.joint3_trans = rendering.Transform(translation=(0, ARM_LEN - ARM_W))
+            self.joint3.add_attr(self.arm2_trans)
+            self.joint3.add_attr(self.joint3_trans)
+            self.joint3.set_color(.5, .5, .8)
+            self.viewer.add_geom(self.joint3)
 
         if self.state is None:return None
         
