3f657d48c65faee36dbd15082d42857b4660db4c
Ming <maximilian-1997@hotmail.com>
2020-06-28 17:30:27 +0800

    Update reward 2 and a sparse reward

--- code/reward_calculate.py
+++ code/reward_calculate.py
@@ -28,10 +28,36 @@ def reward1(finger_pos,state):
 def reward2(finger_pos,state):
     res = 0
     x, y = finger_pos
-    for i in range(3):
-        x_score, y_score = target_points[i]
-        if state[i]: res += -np.sum(np.abs((x,y)-(x_score,y_score)))
-    return res
+    state = list(state)
+    if state == [1,1,1]:
+        x_score, y_score = target_points[0]
+        if not isClose(x,y,x_score,y_score):
+            res += math.exp(-(math.sqrt(math.pow((x - x_score), 2) + math.pow((y - y_score), 2)))*0.0001) - 1.5
+        else:
+            res += 10000
+            state[0] = 0
+    elif state == [0,1,1]:
+        x_score, y_score = target_points[1]
+        if not isClose(x,y,x_score,y_score):
+            res += math.exp(-(math.sqrt(math.pow((x - x_score), 2) + math.pow((y - y_score), 2)))*0.0001) - 1.5
+        else:
+            res += 10000
+            state[1] = 0
+    elif state == [0,0,1]:
+        x_score, y_score = target_points[2]
+        if not isClose(x,y,x_score,y_score):
+            res += math.exp(-(math.sqrt(math.pow((x - x_score), 2) + math.pow((y - y_score), 2)))*0.0001) - 1.5
+        else:
+            res += 10000
+            state[2] = 0
+    elif state == [0,0,0]:
+        x_score, y_score = end_point
+        if not isClose(x,y,x_score,y_score):
+            res += math.exp(-(math.sqrt(math.pow((x - x_score), 2) + math.pow((y - y_score), 2)))*0.0001) - 1.5
+        else:
+            res += 10000
+
+    return res, tuple(state)
 
 def reward3(finger_pos,state):
     res = 0
@@ -52,6 +78,20 @@ def reward3(finger_pos,state):
         x_score,y_score = end_point
         res += math.exp(-(math.sqrt(math.pow((x - x_score), 2) + math.pow((y - y_score), 2)))*0.0001) - 1.5
     return res - sum(state), tuple(state)
+
+def reward_sparse(finger_pos, state):
+    state = list(state)
+    x,y = finger_pos
+    res = 0
+    for i in range(3):
+        if state[i] == 1:
+            x_score, y_score = target_points[i]
+            if isClose(x,y,x_score,y_score):
+                state[i] = 0
+                res += 10000
+                break
+
+    return res, tuple(state)
 
 def reward_test(finger_pos, state):
     return finger_pos[1] - 1000, tuple(state)
